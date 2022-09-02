aadeee24311b417a021818a69f53802787b71651
calvinnickholaa <nickholascalvin1@gmail.com>
2022-09-02 16:41:19 +0700

    admin + sidebar

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,37 @@
+<div class="left-side-bar sidebar-light">
+    <div class="brand-logo">
+        <a href="/">
+            <img src="vendors/images/deskapp-logo.svg" alt="" class="dark-logo" />
+            <img src="vendors/images/deskapp-logo-white.svg" alt="" class="light-logo" />
+        </a>
+        <div class="close-sidebar" data-toggle="left-sidebar-close">
+            <i class="ion-close-round"></i>
+        </div>
+    </div>
+    <div class="menu-block customscroll">
+        <div class="sidebar-menu">
+            <ul id="accordion-menu">
+                <li>
+                    <a href="/ncr" class="dropdown-toggle no-arrow">
+                        <i class="micon bi bi-house"></i> Home
+                    </a>
+                </li>
+                <li>
+                    <a href="/ncr" class="dropdown-toggle no-arrow">
+                        <i class="micon bi bi-people"></i> NCR
+                    </a>
+                </li>
+                <li>
+                    <a href="/kontak" class="dropdown-toggle no-arrow">
+                        <i class="micon bi bi-people"></i> Kontak
+                    </a>
+                </li>
+                <li>
+                    <a href="/memo" class="dropdown-toggle no-arrow">
+                        <i class="micon bi bi-card-heading"></i> Memo
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
