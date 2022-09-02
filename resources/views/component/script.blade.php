eaf79460ea16d5b5cd2996547c196d14a4d7ec8e
tia-ra2112 <tiaralaila21@students.unnes.ac.id>
2022-09-02 15:06:39 +0700

    component style + script 2

--- resources/views/component/script.blade.php
+++ resources/views/component/script.blade.php
@@ -33,10 +33,10 @@
 
 <!-- welcome modal end -->
 <!-- js -->
-<script src="vendors/scripts/core.js"></script>
-<script src="vendors/scripts/script.min.js"></script>
-<script src="vendors/scripts/process.js"></script>
-<script src="vendors/scripts/layout-settings.js"></script>
+<script src="/vendors/scripts/core.js"></script>
+<script src="/vendors/scripts/script.min.js"></script>
+<script src="/vendors/scripts/process.js"></script>
+<script src="/vendors/scripts/layout-settings.js"></script>
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NXZMQSS" height="0" width="0" style="display: none; visibility: hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
