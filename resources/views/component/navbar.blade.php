0b0d07dff759eaa60b0e360cca7022502a7d1ee0
KhoirulAnam-unnes <81313223+KhoirulAnam-unnes@users.noreply.github.com>
2022-09-02 14:37:15 +0700

    component navbar

---
+++ resources/views/component/navbar.blade.php
@@ -0,0 +1,21 @@
+<div class="header">
+    <div class="header-left">
+        <div class="menu-icon bi bi-list"></div>
+        <div class="search-toggle-icon bi bi-search" data-toggle="header_search"></div>
+    </div>
+    <div class="header-right">
+        <div class="user-info-dropdown">
+            <div class="dropdown">
+                <a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                    <span class="user-icon">
+                        <img src="vendors/images/photo1.jpg" alt="" />
+                    </span>
+                    <span class="user-name">Ross C. Lopez</span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                    <a class="dropdown-item" href="login.html"><i class="dw dw-logout"></i> Log Out</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
