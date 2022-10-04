5e9c5e6f9f6ee69e302027d26e011e119aed9579
calvinnickholaa <nickholascalvin1@gmail.com>
2022-10-04 15:41:32 +0700

    add fppp

--- app/Models/Quotation.php
+++ app/Models/Quotation.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Fppp;
+use App\Models\DetailQuotation;
+use App\Models\ProyekQuotation;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Quotation extends Model
 {
     use HasFactory;
     use SoftDeletes;
     protected $table = 'quotations';
+    protected $with = ['Item', 'DataQuotation'];
 
 
     public function Item() 
