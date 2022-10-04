81cae75afbef6acbf667613ebe3081a7244217b8
Ridwan Ridlo <82161307+waniAEbro@users.noreply.github.com>
2022-10-04 13:43:26 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -78,4 +78,4 @@ Route::get("/login", [LoginController::class, "index"])->name("login");
 
 Route::post("/login", [LoginController::class, "login"]);
 
-Route::post("/logout", [LoginController::class, "logout"]);
+Route::post("/logout", [LoginController::class, "logout"])->name("logout");
