4abf598466ec43f11535c3f7a1bfb3ffe17245e5
Ridwan Ridlo <82161307+waniAEbro@users.noreply.github.com>
2022-10-19 08:16:18 +0700

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,12 +12,10 @@ class LoginController extends Controller
     public function index () {
         return redirect("http://erp.alluresystem.site/");
         // Auth::loginUsingId(1);
-        // return redirect("/");
+//         return redirect("/");
     }
 
     public function login (Request $request) {
-        dd($request->ip());
-        
         Auth::loginUsingId(base64_decode($request->user_id));
 
         return redirect("/");
