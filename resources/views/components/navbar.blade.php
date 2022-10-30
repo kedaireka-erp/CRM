47bdb708d487e848f1265befc8a4c76e2e42a035
Ridwan Ridlo <82161307+waniAEbro@users.noreply.github.com>
2022-10-30 10:12:27 +0700

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,5 @@
 <div class="header" style="padding: 0px 15px">
     <div class="header-left">
-        <div class="menu-icon bi bi-list"></div>
-        <div class="search-toggle-icon bi bi-search" data-toggle="header_search"></div>
     </div>
     <div class="header-right">
         <div class="user-info-dropdown">
