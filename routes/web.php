bbcc248da3965861fc7b936c016ec6813c88a799
Ridwan Ridlo <murtiniibu110@gmail.com>
2022-11-30 14:29:23 +0700

    coba

--- routes/web.php
+++ routes/web.php
@@ -27,50 +27,47 @@ Route::middleware("auth")->group(function () {
 
     Route::post("/ncr/validasi/{ncr}", [NcrController::class, "validasi"]);
 
-    Route::middleware("role:Admin|Sales|QC")->group(function () {
+    Route::get('/ncr', [NcrController::class, "index"]);
 
-        Route::get('/ncr', [NcrController::class, "index"]);
+    Route::get('/ncr/create', [NcrController::class, "create"])->middleware("permission:add-ncr");
 
-        Route::get('/ncr/create', [NcrController::class, "create"])->middleware("permission:add-ncr");
+    Route::post('/ncr', [NcrController::class, "store"])->middleware("permission:add-ncr");
 
-        Route::post('/ncr', [NcrController::class, "store"])->middleware("permission:add-ncr");
+    Route::get('/ncr/{ncr}/edit', [NcrController::class, "edit"])->middleware("permission:edit-ncr");
 
-        Route::get('/ncr/{ncr}/edit', [NcrController::class, "edit"])->middleware("permission:edit-ncr");
+    Route::put('/ncr/{ncr}', [NcrController::class, "update"])->middleware("permission:edit-ncr");
 
-        Route::put('/ncr/{ncr}', [NcrController::class, "update"])->middleware("permission:edit-ncr");
+    Route::delete('/ncr/{ncr}', [NcrController::class, "destroy"])->middleware("permission:delete-ncr");
 
-        Route::delete('/ncr/{ncr}', [NcrController::class, "destroy"])->middleware("permission:delete-ncr");
+    Route::get("/kontak", [KontakController::class, "index"]);
 
-        Route::get("/kontak", [KontakController::class, "index"]);
+    Route::get("/kontak/create", [KontakController::class, "create"])->middleware("permission:add-kontak");
 
-        Route::get("/kontak/create", [KontakController::class, "create"])->middleware("permission:add-kontak");
+    Route::post("/kontak", [KontakController::class, "store"])->middleware("permission:add-kontak");
 
-        Route::post("/kontak", [KontakController::class, "store"])->middleware("permission:add-kontak");
+    Route::get("/kontak/{kontak}/edit", [KontakController::class, "edit"])->middleware("permission:edit-kontak");
 
-        Route::get("/kontak/{kontak}/edit", [KontakController::class, "edit"])->middleware("permission:edit-kontak");
+    Route::put("/kontak/{kontak}", [KontakController::class, "update"])->middleware("permission:edit-kontak");
 
-        Route::put("/kontak/{kontak}", [KontakController::class, "update"])->middleware("permission:edit-kontak");
+    Route::delete("/kontak/{kontak}", [KontakController::class, "destroy"])->middleware("permission:delete-kontak");
 
-        Route::delete("/kontak/{kontak}", [KontakController::class, "destroy"])->middleware("permission:delete-kontak");
+    Route::get("/memo", [MemoController::class, "index"]);
 
-        Route::get("/memo", [MemoController::class, "index"]);
+    Route::get("/memo/{ncr}/create", [MemoController::class, "create"])->middleware("permission:add-memo");
 
-        Route::get("/memo/{ncr}/create", [MemoController::class, "create"])->middleware("permission:add-memo");
+    Route::get("/memo/{ncr}/edit", [MemoController::class, "edit"])->middleware("permission:edit-memo");
 
-        Route::get("/memo/{ncr}/edit", [MemoController::class, "edit"])->middleware("permission:edit-memo");
+    Route::put("/memo/{ncr}", [MemoController::class, "update"])->middleware("permission:edit-memo");
 
-        Route::put("/memo/{ncr}", [MemoController::class, "update"])->middleware("permission:edit-memo");
+    Route::delete("/memo/{ncr}", [MemoController::class, "destroy"])->middleware("permission:delete-memo");
 
-        Route::delete("/memo/{ncr}", [MemoController::class, "destroy"])->middleware("permission:delete-memo");
+    Route::post("/memo/{ncr}", [MemoController::class, "store"])->middleware("permission:add-memo");
 
-        Route::post("/memo/{ncr}", [MemoController::class, "store"])->middleware("permission:add-memo");
+    Route::post("/ncr/report", [NcrController::class, "report"]);
 
-        Route::post("/ncr/report", [NcrController::class, "report"]);
+    Route::get("/memo/{ncr}", [MemoController::class, "show"]);
 
-        Route::get("/memo/{ncr}", [MemoController::class, "show"]);
-
-        Route::get('/memo/{ncr}/cetak', [MemoController::class, 'createPDF']);
-    });
+    Route::get('/memo/{ncr}/cetak', [MemoController::class, 'createPDF']);
 });
 
 Route::get("/validate", [NcrController::class, "validate"]);
